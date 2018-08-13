91a98c406802e228caa019a92387715549b3f7cd
Artem Borisov <dedsa2002@gmail.com>
2018-08-13 03:40:24 +0300

    mha: Provide our NFC configuration
    We have a configuration in odm, but it's named libnfc-brcm.conf
    and P expects libnfc-nci.conf. So take our config and copy to system
    with the proper name.

--- lineage_mha.mk
+++ lineage_mha.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# NFC config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
